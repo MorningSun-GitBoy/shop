e349a17627242e4602eb952691adfb8d7d431a6a
HuaWei-01 <AndyWolfcf2013@163.com>
2019-02-17 22:25:18 +0800

    rebuild. 重建

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Router from './router'
-import Store from './store'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(Vant)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- src/store.js
+++ src/store.js
@@ -1,22 +1,16 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    userInfo: {}
-  },
-  getters: {
 
   },
   mutations: {
-    changeLogin(state, status) {
-      state.userInfo = status;
-    }
+
   },
   actions: {
-    loginAction({ commit }, user) {
-      commit('changeLogin', user);
-    }
+
   }
 })
