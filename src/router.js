3ea829d365f689eebe3c887f5410a76b2c9afc89
HuaWei-01 <AndyWolfcf2013@163.com>
2019-02-17 21:52:49 +0800

    keep trying 继续尝试

--- src/router.js
+++ src/router.js
@@ -1,1 +1,65 @@
 import Vue from 'vue'
+import router from 'vue.router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // component: Home
+      components: {
+        default: Home,
+        'footer-bar': FooterBar
+      },
+      meta:{
+        keepAlive: true
+      }
+    }, {
+      path: '/category',
+      name: 'category',
+      // component: Category
+      components: {
+        default: Category,
+        'footer-bar': FooterBar
+      }, 
+      meta:{
+        keepAlive: true
+      }
+    }, {
+      path: '/cart',
+      name: 'cart',
+      // component: Cart
+      components: {
+        default: Cart,
+        'footer-bar': FooterBar
+      },
+      meta:{
+        keepAlive: true
+      }
+    }, {
+      path: '/profile',
+      name: 'profile',
+      component: Profile,
+      components: {
+        default: Profile,
+        'footer-bar': FooterBar
+      },
+      meta:{
+        keepAlive: false
+      }
+    }, {
+      path: '/ajax',
+      name: 'ajax',
+      component: Ajax
+    }, {
+      path: '/detail/:id',
+      name: 'detail',
+      component: Detail
+    }, {
+      path: '*',
+      component: Error
+    }
+  ]
+})
