4b1b958afbbaa3236046a64c854307aee3707fc1
HuaWei-01 <AndyWolfcf2013@163.com>
2019-02-17 21:54:13 +0800

    keep trying 继续尝试

--- src/store.js
+++ src/store.js
@@ -1,1 +1,22 @@
 import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    userInfo: {}
+  },
+  getters: {
+
+  },
+  mutations: {
+    changeLogin(state, status) {
+      state.userInfo = status;
+    }
+  },
+  actions: {
+    loginAction({ commit }, user) {
+      commit('changeLogin', user);
+    }
+  }
+})
